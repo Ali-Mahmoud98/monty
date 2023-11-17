815548f7582732f215668da2bd5e69335fb94518
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-11-17 03:14:14 +0200

    .

--- 7-div.c
+++ 7-div.c
@@ -12,7 +12,7 @@ void divn(stack_t **stack, unsigned int line_number)
 	(void) stack;
 	if (arguments->stack_length < 2)
 	{
-		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
 		free_all_args();
 		exit(EXIT_FAILURE);
 	}
--- 8-mul.c
+++ 8-mul.c
@@ -12,7 +12,7 @@ void mul(stack_t **stack, unsigned int line_number)
 	(void) stack;
 	if (arguments->stack_length < 2)
 	{
-		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		free_all_args();
 		exit(EXIT_FAILURE);
 	}
